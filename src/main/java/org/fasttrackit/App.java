24fdde34eb987cb3c276df375e23178f76b38564
Cristian Mare <pokerstars_42@yahoo.com>
2019-02-20 18:29:56 +0200

    practicing method overloading

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -11,13 +11,18 @@ public class App
 
 
 
-
     Calculator c1 = new Calculator("Bacon Egg & Chees");
     Calculator c2 = new Calculator("Awesomeness");
+
+
 
         System.out.println(c1.getName() + " calculated " + c1.add(3,4) );
         System.out.println(c2.getName() + " calculated " + c2.divide(24,6));
         System.out.println(c1.getName() + " calculated " + c1.multiply(2,5));
+        System.out.println(c2.getName() + " calculated " + c2.add(3.2f,3.3f));
+        System.out.println(c2.getName() + " calculated " + c2.divide(10.5f,2.3f));
+        System.out.println(c1.getName() + " calculated " + c1.multiply(2.4f,4.2f));
+
 
 
 
--- src/main/java/org/fasttrackit/Calculator.java
+++ src/main/java/org/fasttrackit/Calculator.java
@@ -25,14 +25,27 @@ public class Calculator {
     public int add (int x, int y){
         return x + y;
     }
+    public float add (float x, float y) {
+        return x + y;
+    }
 
     public int divide (int x, int y){
         return x / y;
     }
+    public float divide (float x, float y){
+        return x / y;
+    }
+
     public int multiply (int x, int y){
         return x * y;
     }
+    public float multiply (float x, float y){
+        return x * y;
+    }
     public int substract (int x, int y){
+        return x - y;
+    }
+    public float substract (float x, float y){
         return x - y;
     }
 
